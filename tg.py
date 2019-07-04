d1b3a57881a639a7e3f4310f64953be417cb8ead
grepto <grepto@gmail.com>
2019-07-04 16:14:40 +0300

    add new func send_attempts_result() for sending to telegramm

--- tg.py
+++ tg.py
@@ -9,15 +9,11 @@ TG_TOKEN = os.getenv('TG_TOKEN')
 TG_CHAT_ID = os.getenv('TG_CHAT_ID')
 
 
-def post_telegram(message=None, image_path=None):
+def send_message(message=None):
     bot = telegram.Bot(token=TG_TOKEN)
-    if message:
-        bot.send_message(chat_id=TG_CHAT_ID, text=message)
-    if image_path:
-        with open(image_path, 'rb') as image:
-            bot.send_photo(chat_id=TG_CHAT_ID, photo=image)
+    bot.send_message(chat_id=TG_CHAT_ID, text=message)
 
 
 if __name__ == '__main__':
     message = 'Hi!'
-    post_telegram(message)
+    send_message(message)
