94f538bae6fe7281ec1b198c3c589632dce4dc18
grepto <grepto@gmail.com>
2019-07-09 17:21:08 +0300

    fix TelegramHandler class usage in log_config

--- log.py
+++ log.py
@@ -39,7 +39,7 @@ def get_logger(logger_name):
         },
         'loggers': {
             logger_name: {
-                'handlers': ['console'],
+                'handlers': ['console', 'telegram'],
                 'level': LOG_LEVEL,
             }
         },
