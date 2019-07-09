0044b7c5b58b5129a770dc890b60d4d38356ec5f
grepto <grepto@gmail.com>
2019-07-09 23:42:03 +0300

    drop debugging exception

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ def check_devman_attempts():
     while True:
         logger.debug(f'New request with timestamp {params["timestamp"]}')
         try:
-            print(22/0)
             response = requests.get(request_url, headers=headers, params=params, timeout=95).json()
         except requests.exceptions.ReadTimeout as error:
             logger.debug(error)
